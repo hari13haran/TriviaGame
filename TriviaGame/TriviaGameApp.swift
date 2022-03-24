ea76360690307a589c465c8a20f78e0f79c80c16
hari13haran <51775304+hari13haran@users.noreply.github.com>
2022-03-24 19:36:41 +0530

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Hariharan Ganesan on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
