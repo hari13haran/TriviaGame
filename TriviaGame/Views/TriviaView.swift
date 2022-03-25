132c9328f60ee731d833b36fad0357bf1dd7f779
hari13haran <51775304+hari13haran@users.noreply.github.com>
2022-03-25 22:51:56 +0530

    Added NavigationLink between ContentView & QuestionView via TriviaView

---
+++ TriviaGame/Views/TriviaView.swift
@@ -0,0 +1,20 @@
+//
+//  TriviaView.swift
+//  TriviaGame
+//
+//  Created by Hariharan Ganesan on 25/03/22.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+struct TriviaView_Previews: PreviewProvider {
+    static var previews: some View {
+        TriviaView()
+    }
+}
