9164844b5c0d852ae66517c3953b0805df69951d
hari13haran <51775304+hari13haran@users.noreply.github.com>
2022-03-27 22:34:58 +0530

    Added Trivia Model for formatted question & answers array

---
+++ TriviaGame/Models/Trivia.swift
@@ -0,0 +1,52 @@
+//
+//  Trivia.swift
+//  TriviaGame
+//
+//  Created by Hariharan Ganesan on 26/03/22.
+//
+
+import Foundation
+
+struct Trivia: Decodable {
+    var results: [Result]
+    
+    struct Result: Decodable, Identifiable {
+        var id: UUID {
+            UUID()
+        }
+        var category: String
+        var type: String
+        var difficulty: String
+        var question: String
+        var correctAnswer: String
+        var incorrectAnswers: [String]
+        
+
+        var formattedQuestion: AttributedString {
+            do {
+                return try AttributedString(markdown: question)
+            } catch {
+                print("Error setting formattedQuestion: \(error)")
+                return ""
+            }
+        }
+
+        var answers: [Answer] {
+            do {
+                let correct = [Answer(text: try AttributedString(markdown: correctAnswer), isCorrect: true)]
+                let incorrects = try incorrectAnswers.map { answer in
+                    Answer(text: try AttributedString(markdown: answer), isCorrect: false)
+                }
+                
+                let allAnswers = correct + incorrects
+                
+                // Shuffling the answers so the correct answer isn't always the first answer of the array
+                return allAnswers.shuffled()
+                
+            } catch {
+                print("Error setting answers: \(error)")
+                return []
+            }
+        }
+    }
+}
